38043a5cce84a5d727ed0f45936da715b0715c74
Peter van Grieken <hi@frozenrockets.nl>
2014-05-17 21:23:38 +0200

    Bugfixes
    - Safari pixel rounding fix
    - Text changes
    - Contact info

--- src/contact.php
+++ src/contact.php
@@ -74,59 +74,87 @@ if(empty($_POST)) {
 ?>
 <!--(bake html_includes/head.html _section="contact")-->
 
-		<h1>Contact?</h1>
+	<h1>Contact</h1>
 
 
-		<form method="POST" action="contact.php">
+	<div class="grid">
+		<div class="grid__column grid__column--2of3">
+			<form method="POST" action="contact.php">
 
-<?php
-      if ($status === 0) {
-        echo '<p class="status error">Oeps! Het lijkt er op dat niet alle velden ingevuld zijn.</p>';
-      } elseif ($status === -1) {
-        echo '<p class="status error">Oeps! Er ging iets mis aan mijn kant. Kunt u het nog eens proberen?</p>';
-      } elseif ($status === -2) {
-        echo '<p class="status error">Je stuurt 2 berichten in korte tijd (binnen 1 minuut), om spam tegen te gaan mag dat helaas niet.</p>';
-      } elseif ($status === 1) {
-      	echo '<p class="status success">Je bericht is verzonden. Ik zal zo snel mogelijk contact opnemen.</p>';
-      	$hideForm= true;
-      }
+	<?php
+	      if ($status === 0) {
+	        echo '<p class="status error">Oeps! Het lijkt er op dat niet alle velden ingevuld zijn.</p>';
+	      } elseif ($status === -1) {
+	        echo '<p class="status error">Oeps! Er ging iets mis aan mijn kant. Kunt u het nog eens proberen?</p>';
+	      } elseif ($status === -2) {
+	        echo '<p class="status error">Je stuurt 2 berichten in korte tijd (binnen 1 minuut), om spam tegen te gaan mag dat helaas niet.</p>';
+	      } elseif ($status === 1) {
+	      	echo '<p class="status success">Je bericht is verzonden. Ik zal zo snel mogelijk contact opnemen.</p>';
+	      	$hideForm= true;
+	      }
 
-      if(!$hideForm) {
-?>
+	      if(!$hideForm) {
+	?>
+				<p>
+					De snelste manier om contact op te nemen, is door middel van onderstaand formulier.
+					Ik bel of mail zo snel mogelijk terug.
+				</p>
+
+				<label>
+					<span>Naam</span>
+
+					<input type="text" name="name" id="contact-name" pattern="^[a-zA-Z-\' ]{3,50}$" required value="<?php echo $name; ?>">
+					<span class="validation"></span>
+				</label>
+
+				<label>
+					<span>E-mailadres</span>
+
+					<input type="email" name="email" id="contact-email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,5}" required value="<?php echo $email; ?>">
+					<span class="validation"></span>
+				</label>
+
+				<label>
+
+					<span>Uw bericht</span>
+
+					<textarea name="message" id="contact-message" required  value="<?php echo $$_POST['message']; ?>"></textarea>
+
+				</label>
+
+
+				<input type="submit" value="Verstuur" class="button button--secondary js-submit-contact">
+
+
+	<?php
+			}
+	?>
+			</form>
+
+		</div>
+
+		<div class="grid__column grid__column--1of3">
+
+			<h2>Adres</h2>
+
 			<p>
-				De snelste manier om contact op te nemen, is door middel van onderstaand formulier.
-				Ik bel of mail zo snel mogelijk terug.
+				Binckhorstlaan 36<br>
+				2516 BE Den Haag
 			</p>
 
-			<label>
-				<span>Naam</span>
+			<h2>E-mail</h2>
 
-				<input type="text" name="name" id="contact-name" pattern="^[a-zA-Z-\' ]{3,50}$" required value="<?php echo $name; ?>">
-				<span class="validation"></span>
-			</label>
+			<p>
+				<a href="mailto:hi@frozenrockets.nl">hi@frozenrockets.nl</a>
+			</p>
 
-			<label>
-				<span>E-mailadres</span>
+			<h2>Social media</h2>
 
-				<input type="email" name="email" id="contact-email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,5}" required value="<?php echo $email; ?>">
-				<span class="validation"></span>
-			</label>
-
-			<label>
-
-				<span>Uw bericht</span>
-
-				<textarea name="message" id="contact-message" required  value="<?php echo $$_POST['message']; ?>"></textarea>
-
-			</label>
-
-
-			<input type="submit" value="Verstuur" class="button button--secondary js-submit-contact">
-
-
-<?php
-		}
-?>
-		</form>
+			<p>
+				<a href="http://nl.linkedin.com/in/petervangrieken">LinkedIn</a><br>
+				<a href="http://twitter.com/petervangrieken">Twitter</a>
+			</p>
+		</div>
+	</div>
 
 <!--(bake html_includes/footer.html)-->
